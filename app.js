2009dc07e8f8d6ae8c7b903074a75dcaaf91b303
dldPwls03 <lyj3494@naver.com>
2023-06-20 21:07:18 +0900

    코드랑파일추가

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const goodsRouter = require('./routes/goods.js');
+
+const goodsRouter = require("./routes/goods");
+const cartsRouter = require("./routes/carts.js");
+
+const connect = require("./schemas");
+connect();
 
 app.use(express.json());
+app.use("/api", [goodsRouter, cartsRouter]);
 
-app.post("/",(req,res) => {
-  console.log(req.body);
+// const connect = require("./schemas");
+// connect();
 
-  res.send("기본 URI에 POST 메소드가 정상적으로 실행되었습니다.");
-})
+// app.use(express.json());
+// app.use("/api", [goodsRouter]);
 
-app.get("/", (req,res) => {
-  console.log(req.query)
-
-  res.send('정상적으로 변환되었습니다.');
+app.get('/',(req, res) => {
+  res.send('hello world!');
+ // res.send("기본 URI에 POST 메소드가 정상적으로 실행되었습니다.");
 });
 
-app.get("/:id", (req,res) => {
-  console.log(req.params);
+// app.get("/", (req,res) => {
+//   console.log(req.query);
 
-  res.send(":id URI에 정상적으로 변환되었습니다.");
-})
- // app.get('/', (req, res) => {
-//   res.send('Hello World!');
+//   // const obj = {
+//   //   "KeyKey" : "value 입니다.",//K 대문자로 쓰기
+//   //   "이름입니다.":"이름 일까요?",
+//   // }
+  
+//   res.status(200).json({
+//     "KeyKey" : "value 입니다.",
+//     "이름입니다.":"이름일까요?",
+//   }); 
 // });
 
-app.use("/api", goodsRouter);
+// app.get("/:id", (req,res) => {
+//   console.log(req.params);
+
+//   res.send(":id URI에 정상적으로 변환되었습니다.");
+// // })
+//   app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.use("/api", goodsRouter);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
